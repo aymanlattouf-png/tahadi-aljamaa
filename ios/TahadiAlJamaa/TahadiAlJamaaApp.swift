21ebb671fb3538007e800c51275378c1e9d24aa9
aymanlattouf-png <ayman.lattouf@gmail.com>
2026-09-25 22:48:21 +0300

    Add native iOS app entry point

---
+++ ios/TahadiAlJamaa/TahadiAlJamaaApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct TahadiAlJamaaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            GameWebView()
+                .ignoresSafeArea()
+        }
+    }
+}
